e910cf2fed9164be71cc739b6acabe0a283ee8ef
Jackie <78017364+jackiehozi@users.noreply.github.com>
2022-03-22 10:01:00 +0100

    Update question3.sh

--- question3.sh
+++ question3.sh
@@ -9,39 +9,40 @@ echo $nombre
  i = 0
 echo "" > $classe.csv
 
-        while[$i -le $nombre ]; do
+        while[$i -lt $nombre ]; do
             echo "Entrez votre nom"
                 read nom
             echo "Entrez votre prenom"
                 read prenom
-            echo "Entrez votre note"
+            echo "Entrez votre note entre 0 et 20 svp "
                 read note
 
                 if [ $note -eq 20 ];
-                    echo " Vous etes $nom $prenom votre note $note ,C'est parfait !" >> $classe.csv
+                    echo " Vous etes $nom; $prenom; votre note $note ,C'est parfait !" >> $classe.csv
             then
                 
                 elif [ $note -ge 18 ];
-                    echo "Vous etes $nom $prenom votre note $note, C'est parfait" >> $classe.csv
+                    echo "Vous etes $nom ;$prenom ;votre note $note, C'est parfait" >> $classe.csv
             then
                 
                 elif [ $note -ge 16 ];
-                    echo "Vous etes $nom $prenom votre note $note , C'est très bien" >> $classe.csv
+                    echo "Vous etes $nom; $prenom; votre note $note , C'est très bien" >> $classe.csv
             then
                     
                 elif [ $note -ge 14 ];
-                    echo "Vous etes $nom $prenom votre note $note, C'est bien" >> $classe.csv
+                    echo "Vous etes $nom; $prenom ;votre note $note, C'est bien" >> $classe.csv
             then
                     
                 elif [ $note -ge 12 ];
-                    echo "Vous etes $nom $prenom votre note $note, C'est assez bien" >> $classe.csv
+                    echo "Vous etes $nom ;$prenom; votre note $note, C'est assez bien" >> $classe.csv
             then
                     
                 elif [ $note -ge 10 ];
-                    echo "Vous etes $nom $prenom votre note $note, c'est insuffisant" >> $classe.csv
+                    echo "Vous etes $nom ;$prenom; votre note $note, c'est insuffisant" >> $classe.csv
             then
-                    echo " Vous etes $nom $prenom votre note $note, c'est très insuffisant" >> $classe.csv
+                    echo " Vous etes $nom ;$prenom; votre note $note, c'est très insuffisant" >> $classe.csv
             fi
+            i++
         done
 exit
 
